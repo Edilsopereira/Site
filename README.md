1a6fbed4869d678dc10e4ecd3048adb0a543799a
Edilso <115352661+Edilsopereira@users.noreply.github.com>
2024-11-21 14:23:24 -0300

    feet: create page
    Pimeiro site

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Site
-Site Oficial
+# portifolio
+Primeiro portifolio Front-end
+
+## Tarefas
+
+O Controle das tarefas desse projeto sera realizado no GitHub
+
+## Icones 
+- :package: nova funcionalidade \n
+- :up: atualizacao 
+- :beetle: correcao de bugs
+- :checkered_flag: release
+
---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelector(".hamburguer").addEventListener("click", ()=>{
+    document.querySelector(".container").classList.toggle("show-menu")
+});
+
+
+let botao_curriculo = document.querySelector("curriculo")
+ 
+function curriculo(){
+    open("saibamais.html","_self")
+}
